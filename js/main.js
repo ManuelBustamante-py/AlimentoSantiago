f0f4dcb0155405a27cacf4519a4a4240de280165
Manuel <Manuelbustamantealiaga@gmail.com>
2024-07-17 15:58:12 -0400

    nuevo commit

--- js/main.js
+++ js/main.js
@@ -112,22 +112,6 @@
     });
 
 
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
 
 
 
